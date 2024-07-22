0d80c964c31d9f4ef8e5761314afb394fcafcd26
codefish <contact@codefish.io>
2024-07-22 18:08:11 -0500

    Added message from UI

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -5,5 +5,6 @@ public class GitClass {
         System.out.println("Git commit");
         System.out.println("Git push");
         System.out.println("new change");
+        System.out.println("new project UI");
 }
 }
--- src/main/java/GitCommit.java
+++ src/main/java/GitCommit.java
@@ -1,2 +1,5 @@
 public class GitCommit {
+    public static void main(String[] args) {
+        System.out.println("second class");
+    }
 }
